cd82fead629af63a5be144b97b45b1d6f63c2fb0
2026Alma <almanespin09@gmail.com>
2026-06-06 12:51:42 -0600

    class06

--- class 06/main.py
+++ class 06/main.py
@@ -1,14 +1,14 @@
 archivo = open ("C:\\Users\\Persona Invitada\\Desktop\\1-2026-Fundamentos-Python-AlmanP\\class 06\\estudiantes.txt", "a")
-registrar=""
+nombre= str(input("Ingrese su nombre"))
+Carne=int(input("N. de carne"))
+Nota =int (input("Nota final"))
+archivo.write(f"{nombre},{Carne},{Nota}")
+archivo.close()
 while True:
-  nombre= str(input("Ingrese su nombre"))
-  Carne=int(input("N. de carne"))
-  Nota =int (input("Nota final"))
-
-if  == 1 :
-    print(Nombre)
-    print(Carne)
-    print(Nota)
-    archivo.write(Registrar\r)
-archivo.closer()
+    print("1:Registrar estudiante")
+    print("2:Imprimir estudiantes")
+    print("3:Salir")
+    opcion = int(input("Seleccione una opcion"))
+    if opcion == 1
+      print()
 
