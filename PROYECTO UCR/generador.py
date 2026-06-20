d8c54ef85f21b3588bfe52120c88947029d6a9c7
Drake2908-1 <Kjotaro847@gmail.com>
2026-06-20 14:00:03 -0600

    proyect

--- PROYECTO UCR/generador.py
+++ PROYECTO UCR/generador.py
@@ -1,16 +1,121 @@
-import 
-import ramdom
+import random
+letras = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
+caracteres = [
+    "¡", "!", "¿", "?", ",", ";", ".", ":", "-", "_", "/", "()", "(", ")", "[]", "=", "*" 
+]
+numbers = [
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+]
 
+class Contraseña:
+    def analizar(self, contraseña):
+        puntos = 0
+        if len(contraseña) >= 8:
+            puntos += 1
+            print("✓ La contraseña si tiene al menos 8 caracteres.")
+        else:
+            print("✗ La contraseña tiene menos de 8 caracteres.")
 
+        tiene_mayuscula = False
+        for letra in contraseña:
+            if letra.isupper():
+                tiene_mayuscula = True
 
+        if tiene_mayuscula:
+            puntos += 1
+            print("✓ La contraseña contiene letras mayúsculas.")
+        else:
+            print("X La contraseña no contiene letras mayúsculas.")
+        
+        tiene_minuscula = False
+        for letra in contraseña:
+            if letra.islower():
+                tiene_minuscula = True
 
-def contrasena_segura(extension):
-    formato=sting.digits+signos.especiales+numeros.n0
-    contrasena = " "
-    for i in range (extension):
-        contrasena += ramdom.choice(contrasena)
+        if tiene_minuscula:
+            puntos += 1
+            print("✓ La contraseña contiene letras minúsculas.")
+        else:
+            print("X La contraseña no contiene letras minúsculas.")
+        
+        tiene_numero = False
+        for letra in contraseña:
+            if letra.isdigit():
+                tiene_numero = True
+
+        if tiene_numero:
+            puntos += 1
+            print("✓ La contraseña contiene números.")
+        else:
+            print("✗ La contraseña no contiene números.")
+
+        tiene_caracter_especial = False
+        caracteres_especiales = "!@#$%^&*()-_=+[]{};:,.<>?/"
+        for letra in contraseña:
+            if letra in caracteres_especiales:
+                tiene_caracter_especial = True
+
+        if tiene_caracter_especial:
+            puntos += 1
+            print("✓ La contraseña contiene caracteres especiales.")
+        else:
+            print("✗ La contraseña no contiene caracteres especiales.") 
+            print ("Caracteres permitidos: !@#$%^&*()-_=+[]{};:,.<>?/")
+            
+        print("\n--- RESUMEN DEL ANÁLISIS ---")
+        
+        if puntos == 5:
+            print("Resultado: ¡Contraseña Muy Segura! 💪")
+        elif puntos >= 3:
+            print("Resultado: Contraseña Aceptable. ⚠️ Podría mejorar.")
+        else:
+            print("Resultado: Contraseña Insegura. ❌ ¡Cambiala!")
+        return contraseña
+
+    def generar(self):
+        contrasena = " "
+        for i in range(4):
+            letra = random.choice(letras)
+            if i < 2:
+                contrasena += letra.upper()
+            else:
+                contrasena += letra.lower()
+        for i in range(4):
+            contrasena += random.choice(caracteres)
+        for i in range(4):
+            contrasena += random.choice(numbers)
         return contrasena
+        
+    
 
-extension=int(input("Cual es la extensión deseada en la contraseña"))
-contrasena_lista = contrasena_segura(extension)
-print("La nueva contraseña es:",contrasena_lista)
+    
+
+    
+
+contraseñas = []
+contrasena = Contraseña()
+           while True:
+            print("Bienvenido al analizador de contraseñas seguras")
+            print("1. Analizar contraseña")
+            print("2. Ver contraseñas analizadas")
+            print("3. Generar contraseña segura")
+            print("4. Salir")
+
+            opcion = int(input( "Seleccione una opción:" ))
+            if opcion == 1:
+            contraseña = input("Ingrese la contraseña a analizar: ")
+            contrasena.analizar(contraseña)
+            contraseñas.append(contraseña)
+            elif opcion == 2:
+            print("Contraseñas analizadas:", contraseñas)
+            elif opcion == 3 :
+            contrasena.generar()
+            print("Contraseña segura generada:  ", contrasena.generar())
+            break
+
+        elif opcion == 4:
+                 print("Gracias por usar el analizador de contraseñas seguras. ")
+            break
+            print("Opción no valida. Por favor seleccione una opción del 1 al 3.")
